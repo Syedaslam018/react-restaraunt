8f9c42b17fd245a1f250f7100d8221fb6e554431
Syed Aslam <syedaslam@Syeds-MacBook-Air.local>
2025-03-17 19:13:02 +0530

    add card button

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
 const MealItem = props => {
     let price = `$${props.price}`
   return (
     <li>
-        <div className={classes.meal}>
+      <div className={classes.meal}>
+        <div >
             <h3>{props.name}</h3>
             <div className={classes.description}>{props.description}</div>
             <div className={classes.price}>{price}</div>
         </div>
-        <div></div>
+        <div>
+          <MealItemForm id={props.id}/>
+        </div>
+        </div>
     </li>
   )
 }
