9b82474d767c4afd62ebacd615b921afd6c9dd82
Syed Aslam <syedaslam@Syeds-MacBook-Air.local>
2025-03-17 20:05:30 +0530

    hard code cart

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Fragment } from 'react'
+import classes from './Modal.module.css'
+import ReactDOM from 'react-dom'
+
+const Backdrop = props => {
+    return (
+        <div className={classes.backdrop}></div>
+    )
+}
+const ModalOverlay = props => {
+    return (
+            <div className={classes.modal}>
+                <div className={classes.content}> {props.children}</div>
+            </div>
+            
+        
+    )
+}
+
+const Modal = props => {
+    const protalElement = document.getElementById('overlays')
+  return (
+   <Fragment>
+   {ReactDOM.createPortal(<Backdrop />, protalElement)}
+   {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, protalElement)}
+   </Fragment>
+  )
+}
+
+export default Modal
